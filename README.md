b4095979af611f69d1a2db61d7231ea3d2cde938
Darrrah <darashalabi@gmail.com>
2020-02-19 03:07:44 +0200

    this is lab03

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # project2-aboutme
 about-me-project
 
+## This is a regularly updated page
+*Last update Wed Feb 19*
 
 ### Project Title
 About-Me, with 'A Guessing Game'
@@ -8,6 +10,9 @@ About-Me, with 'A Guessing Game'
 This is Darah, and this website is about me. 
 
 From this point forward, you are going to learn more about ME.
+
+In the 3rd lab, some edits were made. HTML file, CSS file & the JS file were all modified and edits were pushed successfully up to the cloud on Feb 19.
+Using new features to make the site more interactive and look better!
 **This Project Was Built With**
 - JavaScript
 - HTML
@@ -16,3 +21,4 @@ From this point forward, you are going to learn more about ME.
 Darah Shalabi - Initial work - Darah98
 **License**
 This project is licensed under the MIT License - see the LICENSE.md file for details
+
--- js/app.js
+++ js/app.js
@@ -1,62 +1,112 @@
-'use strict';
-var visitorName = prompt('WELCOME visitor! What is your name?');
-alert('Thank you for visiting ' + visitorName + '. We will get to know each other more!');
-var knowMore = prompt('Are you ready for a little Ice Breaker?').toLowerCase();
-if (knowMore === 'yes' || knowMore === 'y'){
-  //console.log('Sounds Great! Let us get started.');
-  alert('Sounds Great! Let us get started.');
-} else if (knowMore === 'no' || knowMore === 'n'){
-  //console.log('Too bad! Get ready.');
-  alert('Too bad! Get ready.');
-} else {
-  //console.log('Please answer yes/no');
-  alert('Please answer yes/no');
-}
-var scndQ = prompt('Do you think I like the singer Post Malone?').toLowerCase();
-if (scndQ === 'yes' || scndQ === 'y'){
-  //console.log('You answered correctly.');
-  alert('You answered correctly.');
-} else if (scndQ === 'no' || scndQ === 'n'){
-  //console.log('Wrong answer. I do!');
-  alert('Wrong answer. I do!');
-} else {
-  //console.log('Please answer yes/no');
-  alert('Please answer yes/no');
-}
-var thrdQ = prompt('Do I wear glasses?').toLowerCase();
-if (thrdQ === 'no' || thrdQ === 'n'){
-  //console.log('You answered correctly.');
-  alert('You answered correctly.');
-} else if (thrdQ === 'yes' || thrdQ === 'y'){
-  //console.log('Wrong answer. I dont!');
-  alert('Wrong answer. I dont!');
-} else {
-  //console.log('Please answer yes/no');
-  alert('Please answer yes/no');
-}
-var frthQ = prompt('Is my hair long & black?').toLowerCase();
-if (frthQ === 'yes' || frthQ === 'y'){
-  //console.log('You answered correctly.');
-  alert('You answered correctly.');
-} else if (frthQ === 'no' || frthQ === 'n'){
-  //console.log('Wrong answer. It is!');
-  alert('Wrong answer. It is!');
-} else {
-  //console.log('Please answer yes/no');
-  alert('Please answer yes/no');
-}
-var fifthQ = prompt('Do I like languages?').toLowerCase();
-if (fifthQ === 'yes' || fifthQ === 'y'){
-  //console.log('You answered correctly.');
-  alert('You answered correctly.');
-} else if (fifthQ === 'no' || fifthQ === 'n'){
-  //console.log('Wrong answer. I do!');
-  alert('Wrong answer. I do!');
-} else {
-  //console.log('Please answer yes/no');
-  alert('Please answer yes/no');
-}
-alert('Thank you ' + visitorName + ' for taking part in this. We hope to see you again soon!');
+// 'use strict';
+// var guestScore = 0;
+// var visitorName = prompt('WELCOME visitor! What is your name?');
+// alert('Thank you for visiting ' + visitorName + '. We will get to know each other more!');
+// var knowMore = prompt('Are you ready for a little Ice Breaker?   (Please enter yes/no OR y/n)').toLowerCase();
+// if (knowMore === 'yes' || knowMore === 'y'){
+//   alert('Sounds Great! Let\'s get started.');
+//   guestScore++;
+// } else if (knowMore === 'no' || knowMore === 'n'){
+//   alert('Too bad! Get ready.');
+// } else {
+//   alert('Please answer yes/no');
+// }
+// console.log(knowMore);
+// console.log(guestScore);
+// var scndQ = prompt('Do you think I like the singer Post Malone?   (Please enter yes/no OR y/n)').toLowerCase();
+// if (scndQ === 'yes' || scndQ === 'y'){
+//   alert('You answered correctly.');
+//   guestScore++;
+// } else if (scndQ === 'no' || scndQ === 'n'){
+//   alert('Wrong answer. I do!');
+// } else {
+//   alert('Please answer yes/no');
+// }
+// console.log(scndQ);
+// console.log(guestScore);
+// var thrdQ = prompt('Do I wear glasses?   (Please enter yes/no OR y/n)').toLowerCase();
+// if (thrdQ === 'no' || thrdQ === 'n'){
+//   alert('You answered correctly.');
+//   guestScore++;
+// } else if (thrdQ === 'yes' || thrdQ === 'y'){
+//   alert('Wrong answer. I dont!');
+// } else {
+//   alert('Please answer yes/no');
+// }
+// console.log(thrdQ);
+// console.log(guestScore);
+// var frthQ = prompt('Is my hair long & black?   (Please enter yes/no OR y/n)').toLowerCase();
+// if (frthQ === 'yes' || frthQ === 'y'){
+//   alert('You answered correctly.');
+//   guestScore++;
+// } else if (frthQ === 'no' || frthQ === 'n'){
+//   alert('Wrong answer. It is!');
+// } else {
+//   alert('Please answer yes/no');
+// }
+// console.log(frthQ);
+// console.log(guestScore);
+// var fifthQ = prompt('Do I like languages?   (Please enter yes/no OR y/n)').toLowerCase();
+// if (fifthQ === 'yes' || fifthQ === 'y'){
+//   alert('You answered correctly.');
+//   guestScore++;
+// } else if (fifthQ === 'no' || fifthQ === 'n'){
+//   alert('Wrong answer. I do!');
+// } else {
+//   alert('Please answer yes/no');
+// }
+// console.log(fifthQ);
+// console.log(guestScore);
 
-var inviteFrnd= document.getElementById('inviteFrnd');
-inviteFrnd.append('Thank you for taking this quiz ' + visitorName + '! Do not hesitate to approach me!');
+// alert('Thank you ' + visitorName + ' for taking part in this Get2KnowMe game. Hope we got to break the ice already!');
+// alert('Now it\'s time for a little guessing game ' + visitorName + '! You will try to guess my age to get access! I will give you hints ;)');
+
+// var visitorGuess = prompt('Please note you have 4 chances to guess right! Go ahead and take your first guess:');
+// for (let i = 0; i < 4; i++) {
+//   visitorGuess = parseInt(visitorGuess);
+//   console.log(visitorGuess);
+//   if (visitorGuess === 21) {
+//     guestScore++;
+//     alert('You are correct!');
+//     break;
+//   } else if (visitorGuess > 19 && visitorGuess < 23 && visitorGuess !== 21) {
+//     visitorGuess= prompt('You were very close! Please take another guess:');
+//   } else if (visitorGuess >= 23) {
+//     visitorGuess= prompt('Your guess was too high! Please take another guess:');
+//   } else if (visitorGuess <= 19) {
+//     visitorGuess= prompt('Your guess was too low! Please take another guess:');
+//   } else {
+//     prompt('Are you still there? Guess age here:');
+//   }
+//   if (i >= 3) {
+//     alert('Hard Luck! I\'m 21');
+//   }
+// }
+// console.log(guestScore);
+
+// var singerList = ['lil wayne' , 'kendrick lamar' , 'post malone' , 'the weeknd'];
+// var favSinger = prompt('Just one more step and you get there! For this question, you have 6 attempts to guess one of my favourite singers. Take a wild guess:').toLowerCase();
+// var x = -1;
+// for (var r = 0 ; r < 7 ; r++) {
+//   for (var s = 0 ; s < singerList.length ; s++){
+//     if (favSinger === singerList[s]) {
+//       x = s;
+//       break;
+//     }
+//   }
+//   console.log(x);
+//   if (x > -1) {
+//     alert('You are right! The singer names were: ' + singerList);
+//     guestScore++;
+//     console.log(guestScore);
+//     break;
+//   } else {
+//     favSinger = prompt('Sorry, you have to try again!');
+//   }
+// }
+// if (x === -1) {
+//   alert('The singer names were: ' + singerList);
+// }
+// alert('Thank you for baring with me, ' + visitorName + '! Your final score is: ' + guestScore + '/7');
+// var inviteFrnd= document.getElementById('inviteFrnd');
+// inviteFrnd.append('Thank you for visiting, ' + visitorName + '! Hope to see you again so soon! Do not hesitate to approach me!');
